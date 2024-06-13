f16c5265fa5eddd90333efac12267615c24da2e2
luisafab <luisafaber@rocketmail.com>
2024-06-13 10:23:36 +0200

    add calculation of invariant mass

---
+++ massPlot.c
@@ -0,0 +1,92 @@
+#include <ROOT/RDataFrame.hxx>
+#include <math.h> 
+
+void massPlot(TString file="data/AnalysisResults_treesAP_data.root", TString tree="O2v0tableap") {
+
+    float massProton=0.9382720813;
+    float massPion= 0.13957061;
+
+    // define a data frame to use 
+    ROOT::RDataFrame df(tree, file);
+
+
+    // lambda functions to calculate the variables of the AP Plot: alpha and pT
+    // takes the momenta of the negative and positive particle as argument
+    auto alpha = [&](float posx, float posy, float posz, float negx, float negy, float negz){
+        // create momentum vector for both particles
+        TVector3 pPos(posx,posy,posz);
+        TVector3 pNeg(negx,negy,negz);
+        // the momentum of V0 is the sum of the other two
+        TVector3 pV0 =pPos+pNeg;
+        // normalize pV0
+        float pV0_norm=sqrt(pV0.Dot(pV0));
+        // calculate longitudinal part of pPos and pNeg
+        float pPlL = pPos.Dot(pV0)/pV0_norm;
+        float pNegL = pNeg.Dot(pV0)/pV0_norm;
+        // calculate alpha
+        float alpha= (pPlL-pNegL)/(pPlL+pNegL);
+
+        return alpha;
+    };
+
+    // define new columns with the two new variables 
+    auto new_column = df.Define("alpha", alpha, {"fPxPos", "fPyPos","fPzPos","fPxNeg", "fPyNeg","fPzNeg"});
+
+    // calculate the invariant mass 
+    // given variables : momenta and alpha 
+
+    auto invariantmass = [&](float alpha, float posx, float posy, float posz, float negx, float negy, float negz){
+        float massPos;
+        float massNeg;
+
+        //define vectors for momenta and calclate the length 
+        TVector3 pPos(posx,posy,posz);
+        TVector3 pNeg(negx,negy,negz);
+        TVector3 momentum=pPos+pNeg;
+        float pPos_norm=sqrt(pPos.Dot(pPos));
+        float pNeg_norm=sqrt(pNeg.Dot(pNeg));
+        float momentum_norm=sqrt(momentum.Dot(momentum));
+
+
+        // use alpha to know the decay and the resulting particles
+        // alpha>0 lambda -> p +pi-
+        // alpha<0 pi+
+        // assign the mass 
+        if (alpha>0){
+            massPos=massProton;
+            massNeg=massPion;
+        }
+        else{
+            massPos=massPion;
+            massNeg=massProton;
+        }
+
+        // use mass and momentum to calculate the energy of the two particles
+        float EPos=sqrt(pow(pPos_norm,2.)+pow((massPos),2.));
+        float ENeg=sqrt(pow(pNeg_norm,2.)+pow((massNeg),2.));
+
+        // total energy 
+        float Energy = EPos + ENeg;
+
+        // calculate the invariant mass using the fourmomentum vector
+        float invariantmass = sqrt((pow(Energy,2.)-pow(momentum_norm,2.)));
+
+        return invariantmass;
+    };
+
+    // get the final data frame
+    auto complete_df = new_column.Define("invariantmass", invariantmass, {"alpha", "fPxPos", "fPyPos","fPzPos","fPxNeg", "fPyNeg","fPzNeg"});
+
+    // plot the two new variables
+    auto h1 = complete_df.Histo1D("alpha");
+    TCanvas * c1 = new TCanvas("c1", "c1", 600, 600);
+    h1->DrawClone();
+    gROOT->GetListOfCanvases()->Draw();
+
+    auto h2 = complete_df.Histo1D("invariantmass");
+    TCanvas * c2 = new TCanvas("c2", "c2", 600, 600);
+    h2->DrawClone();
+    gROOT->GetListOfCanvases()->Draw();
+
+
+}
