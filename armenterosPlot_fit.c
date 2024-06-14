e5f4fb97d9dcf79c0bc3f41863537734680e9414
luisafab <luisafaber@rocketmail.com>
2024-06-14 11:11:15 +0200

    add calculation of invariant mass

--- armenterosPlot_fit.c
+++ armenterosPlot_fit.c
@@ -8,6 +8,8 @@
 
 void armenterosPlot_fit(TString file="data/AnalysisResults_treesAP_data.root", TString tree="O2v0tableap",bool MC=false) {
 
+    float massProton=0.9382720813;
+    float massPion= 0.13957061;
 
     // define a data frame to use 
     ROOT::RDataFrame df_MC(tree, file);
@@ -52,11 +54,87 @@ void armenterosPlot_fit(TString file="data/AnalysisResults_treesAP_data.root", T
     };
 
     // define new columns with the two new variables 
-    auto new_column = df.Define("alpha", alpha, {"fPxPos", "fPyPos","fPzPos","fPxNeg", "fPyNeg","fPzNeg"});
+    auto new_column_alpha = df.Define("alpha", alpha, {"fPxPos", "fPyPos","fPzPos","fPxNeg", "fPyNeg","fPzNeg"});
     // get the final data frame
-    auto complete_df = new_column.Define("pT", pT, {"fPxPos", "fPyPos","fPzPos","fPxNeg", "fPyNeg","fPzNeg"});
+    auto new_column_pT = new_column_alpha.Define("pT", pT, {"fPxPos", "fPyPos","fPzPos","fPxNeg", "fPyNeg","fPzNeg"});
 
-    // plot the two new variables
+    // calculation of the invariant mass of lambdas
+    auto invariantmass_lambda = [&](float alpha, float posx, float posy, float posz, float negx, float negy, float negz){
+        float massPos;
+        float massNeg;
+
+        //define vectors for momenta and calclate the length 
+        TVector3 pPos(posx,posy,posz);
+        TVector3 pNeg(negx,negy,negz);
+        TVector3 momentum=pPos+pNeg;
+        float pPos_norm=sqrt(pPos.Dot(pPos));
+        float pNeg_norm=sqrt(pNeg.Dot(pNeg));
+        float momentum_norm=sqrt(momentum.Dot(momentum));
+
+
+        // use alpha to know the decay and the resulting particles
+        // alpha>0 lambda -> p +pi-
+        // alpha<0 pi+
+        // assign the mass 
+        if (alpha>0){
+            massPos=massProton;
+            massNeg=massPion;
+        }
+        else{
+            massPos=massPion;
+            massNeg=massProton;
+        }
+
+        // use mass and momentum to calculate the energy of the two particles
+        float EPos=sqrt(pow(pPos_norm,2.)+pow((massPos),2.));
+        float ENeg=sqrt(pow(pNeg_norm,2.)+pow((massNeg),2.));
+
+        // total energy 
+        float Energy = EPos + ENeg;
+
+        // calculate the invariant mass using the fourmomentum vector
+        float invariantmass = sqrt((pow(Energy,2.)-pow(momentum_norm,2.)));
+
+        return invariantmass;
+    };
+
+    //calculation of the invariant mass of K0s
+    auto invariantmass_K0 = [&](float posx, float posy, float posz, float negx, float negy, float negz){
+        float massPos;
+        float massNeg;
+
+        //define vectors for momenta and calclate the length 
+        TVector3 pPos(posx,posy,posz);
+        TVector3 pNeg(negx,negy,negz);
+        TVector3 momentum=pPos+pNeg;
+        float pPos_norm=sqrt(pPos.Dot(pPos));
+        float pNeg_norm=sqrt(pNeg.Dot(pNeg));
+        float momentum_norm=sqrt(momentum.Dot(momentum));
+
+
+        // decays in pi+ and pi-, masses are equal
+        massPos=massPion;
+        massNeg=massPion;
+
+        // use mass and momentum to calculate the energy of the two particles
+        float EPos=sqrt(pow(pPos_norm,2.)+pow((massPos),2.));
+        float ENeg=sqrt(pow(pNeg_norm,2.)+pow((massNeg),2.));
+
+        // total energy 
+        float Energy = EPos + ENeg;
+
+        // calculate the invariant mass using the fourmomentum vector
+        float invariantmass = sqrt((pow(Energy,2.)-pow(momentum_norm,2.)));
+
+        return invariantmass;
+    };
+
+    // add both masses as new column
+    auto new_minv_l = new_column_pT.Define("Minv_lambda", invariantmass_lambda, {"alpha", "fPxPos", "fPyPos","fPzPos","fPxNeg", "fPyNeg","fPzNeg"});
+    auto complete_df = new_minv_l.Define("Minv_K0", invariantmass_K0, {"fPxPos", "fPyPos","fPzPos","fPxNeg", "fPyNeg","fPzNeg"});
+
+
+    // plot the different variables
     auto h1 = complete_df.Histo1D("alpha");
     TCanvas * c1 = new TCanvas("c1", "c1", 600, 600);
     h1->DrawClone();
@@ -67,6 +145,17 @@ void armenterosPlot_fit(TString file="data/AnalysisResults_treesAP_data.root", T
     h2->DrawClone();
     gROOT->GetListOfCanvases()->Draw();
 
+    auto h3 = complete_df.Histo1D("Minv_K0");
+    TCanvas * c3 = new TCanvas("c3", "c3", 600, 600);
+    h3->DrawClone();
+    gROOT->GetListOfCanvases()->Draw();
+
+    auto h4 = complete_df.Histo1D("Minv_lambda");
+    TCanvas * c4 = new TCanvas("c4", "c4", 600, 600);
+    h4->DrawClone();
+    gROOT->GetListOfCanvases()->Draw();
+
+
     // define and plot a histogram which is the armenteros plot
     TH2D *h = new TH2D("h","Armenteros-Podolanski Plot",100,-1.,1.,40,0.,0.2);
     
@@ -75,7 +164,7 @@ void armenterosPlot_fit(TString file="data/AnalysisResults_treesAP_data.root", T
     histo->GetXaxis()->SetTitle("alpha");
     histo->GetYaxis()->SetTitle("pT");
 
-    TCanvas * c3 = new TCanvas("c3", "c3", 900, 600);
+    TCanvas * c = new TCanvas("c", "c", 900, 600);
     histo->DrawClone();
     gROOT->GetListOfCanvases()->Draw();
 
