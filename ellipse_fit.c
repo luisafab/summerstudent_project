66904a6fc2e97b28424bf32d7fd0e95e668c9526
luisafab <luisafaber@rocketmail.com>
2024-07-30 10:52:21 +0200

    add pol2 fit to minimum and use beta as argument instead of pT bin and add couts

--- ellipse_fit.c
+++ ellipse_fit.c
@@ -2,22 +2,18 @@
 #include <Math/RootFinder.h>
 #include <Math/Functor.h>
 // function to calculate ellipse qT for given masses M and m 
-double qT(double alpha, double M, double m, double beta){
+double qT(double alpha, double M, double m, double beta_pT){
     // including a factor beta to compensate a lower beta at lower pT
-    alpha=alpha*beta;
+    alpha=alpha*beta_pT;
     return sqrt(pow(M,2.)/4*(1-pow(alpha,2.))-pow(m,2.));
 };
 // distance of given point x,y to ellipse at point alpha
 // euclidean distance: sqrt((x1-x2)^2+(y1-y2)^2)
-double distance(double alpha, double M, double m, double x, double y, double beta){
-    double qT_alpha=qT(alpha,M,m,beta);
+double distance(double alpha, double M, double m, double x, double y, double beta_pT){
+    double qT_alpha=qT(alpha,M,m,beta_pT);
     return sqrt(pow((qT_alpha-y),2.)+pow((alpha-x),2.));
 }
-// calculate beta for different pT (pT=m*gamma*beta)
-double beta(double pT){
-    double M=0.497164;
-    return sqrt(1./(pow((M/pT),2.)+1.));
-}
+
 
 // function to calculate the distance to an ellipse with given m for APPlot saved in data frame with number of bins nx and ny
 double fit_ellipse (ROOT::RDataFrame df, double M, double nx, double ny, double beta_pT){
@@ -42,7 +38,7 @@ double fit_ellipse (ROOT::RDataFrame df, double M, double nx, double ny, double 
     axis->SetLimits(-1.,1.);
     graph->GetHistogram()->SetMinimum(-0.02);
     graph->GetHistogram()->SetMaximum(0.25);
-    graph->Write(Form("Ellipse_%.6lf",M));
+    //graph->Write(Form("Ellipse_%.6lf",M));
 
     // Histogram for AP Plot
     // use given binning
@@ -111,28 +107,18 @@ double fit_ellipse (ROOT::RDataFrame df, double M, double nx, double ny, double 
             sumOfDistance+=smallestsquared;
         }
     }
-    // plot results
-    gr->Write();
-    wd->Write();
-    //std::cout<<sumOfDistance<<std::endl;
+    // return result
     return sumOfDistance;
 }
 
 // file, tree: name of file and tree with variables calculated for the AP-plot
-void ellipse_fit(TString file="APPlot_df_lowPt.root", TString tree="NewVariables",TString output="results_ellipse_fit_finerBinning_alpha09.root", double pT=4.) {
-    //double m=0.13957061;
+double ellipse_fit(TString file="APPlot_df_lowPt.root", TString tree="NewVariables",TString output="results_ellipse_fit_finerBinning_alpha09.root", double beta_pT=0.9) {
+    double m=0.13957061;
     double M=0.497164;
-    // calculate beta for given pT to adapt in qT calculation
-    double beta_pT=beta(pT);
-    std::cout<<"calculate qT using beta (pT="<<pT<<") = "<<beta_pT<<std::endl;
+    
+    std::cout<<"calculate qT using beta  = "<<beta_pT<<std::endl;
     // stop time of the iteration
     TStopwatch watch;
-    
-    // saving plots in a root file
-    std::unique_ptr<TFile> myFile( TFile::Open(output,"RECREATE") );
-    // create subdirectory to save files 
-    myFile->cd();
-    gDirectory->mkdir("DifferentMasses");
 
     // get DataFrame out of file 
     ROOT::RDataFrame df(tree, file);
@@ -142,23 +128,22 @@ void ellipse_fit(TString file="APPlot_df_lowPt.root", TString tree="NewVariables
     int ny=1000;
     TH2D *h = new TH2D("AP_2D","Armenteros-Podolanski Plot; #alpha; q_{T}; z",nx,-1.,1.,ny,0.,0.25);
     auto histo = df.Histo2D(*h,"alpha","qT");
-    // plot
+    //plot
     histo->Write();
+
     // iterate over n different masses around the k0s mass M
-    int n=100;
-    double dist=0.2/n;
+    int n=50;
+    std::cout<<"calculate error for "<<n<<" different masses"<<std::endl;
+    double dist=0.1/n;
     // save different masses and calculated smallest distance
     double error[n];
     double masses[n];
     for (int i=0; i<n; i++){
-        myFile->cd();
-        myFile->cd("DifferentMasses");
-        M=0.497164-0.08+i*dist;
+        M=0.497164-0.04+i*dist;
         error[i]=fit_ellipse(df,M,nx,n,beta_pT);
         masses[i]=M*1000; // MeV
     }
     // plot for different masses 
-    myFile->cd();
     auto gr = new TGraph(n,masses,error);
     gr->SetTitle("Sum of distances for different masses of K^{0}_{s}");
     gr->GetXaxis()->SetTitle("M [MeV]");
@@ -167,11 +152,40 @@ void ellipse_fit(TString file="APPlot_df_lowPt.root", TString tree="NewVariables
     gr->SetMarkerSize(1.5);
     gr->SetMarkerStyle(70);
     gr->GetHistogram()->SetMinimum(-0.02);
-    gr->Write("Result");
     
-    myFile->Close();    // close file
 
-    std::cout<<watch.RealTime()<<std::endl;     // print time 
+    std::cout<<"Fit pol2 to area around the minimum"<<std::endl;
+    // try to fit pol2 to mass plot
+    TF1 *f1 = new TF1("f1", "pol2");
+    gr->Fit(f1,"","",470.,520.);
+    double p0 = f1->GetParameter(0);
+    double p1 = f1->GetParameter(1);
+    double p2 = f1->GetParameter(2);
+    gr->Write("Result");
+    double center=-p1/(2*p2);
+
+    // plot matched ellipse
+    // calculate values for corresponding ellipse
+    int n2=160;
+    double alpha_plot[n2];
+    double qT_plot[n2];
+    for (int l=0; l<n2; l++){
+        double alpha_temp=(-0.8+l*0.01);
+        alpha_plot[l]=alpha_temp;
+        qT_plot[l]=qT(alpha_temp,center/1000,m,beta_pT);
+    }
+    auto ellipse = new TGraph(n2,alpha_plot,qT_plot);
+    ellipse->SetTitle(Form("Ellipse with #beta = %.3lf",beta_pT));
+    ellipse->GetXaxis()->SetTitle("#alpha");
+    ellipse->GetYaxis()->SetTitle("q_{T}");
+    ellipse->SetLineWidth(3);
+    ellipse->SetLineColor(2);
+    ellipse->Write("Ellipse");
+
+    std::cout<<"Fitted ellipse to AP-plot with resulting mass M="<<center<<" MeV"<<std::endl;
     
+    std::cout<<"Time needed: "<<watch.RealTime()<<std::endl;     // print time 
+    
+    return center;
 
 }
